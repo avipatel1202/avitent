f4f01513ec5010b29bafffca129fd63b5ff67e91
Avi <avirpatel1202@gmail.com>
2022-06-06 17:00:29 +0530

    halfway there

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import About from "./About";
 import Connect_wallet from "./components/Connect_wallet";
+import OpenTent from "./components/OpenTent";
+
 
 function App() {
 
   return (
     <div className="App">
-      {/*<About />*/}
-      <Connect_wallet />
+      {/*<About />
+      <Connect_wallet />*/}
+      <OpenTent/>
+      
     </div>
   );
 }
